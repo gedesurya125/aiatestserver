776055522f9ed493e75c62454bc71882355dad10
gedesurya125 <gedesurya125@gmail.com>
2021-10-29 15:16:52 +0800

    fixing test

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # AIA TEST SERVER (BACK END)
 by I Gede Surya Adi Pranata
+
+How to run in development mode:
+```npm run dev```
+
+How to run in production mode:
+```npm run start```
+
+How to run test
+```npm run test```
+
--- test/flickr.test.js
+++ test/flickr.test.js
@@ -1,12 +1,22 @@
 const request = require("supertest");
 const app = require("../index.js");
 
+
+describe("test root route", () => {
+  it('should redirect to postman documentation', async() => {
+    const res = await request(app).get('/').redirects(0).expect(302);
+    expect(res.headers.location).toMatch(/^https:\/\/documenter\.getpostman\.com.*$/gi);
+  })
+})
+
 describe("Random fetching", () => {
   it("get random fetching", async () => {
     const res = await request(app).get("/random");
     expect(res.statusCode).toEqual(404);
   });
 });
+
+
 
 describe("Get All image from flickr", () => {
   it("get all image", async () => {
